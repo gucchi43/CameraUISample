ff2848b3b02a301df436c7829f3d438fd855ef09
Hiroki Taniguchi <acmikn43@gmail.com>
2017-02-19 02:44:33 +0900

    Initial Commit

---
+++ CameraUISample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraUISample
+//
+//  Created by HIroki Taniguti on 2017/02/19.
+//  Copyright © 2017年 HIroki Taniguti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
